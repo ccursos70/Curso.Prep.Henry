686df59631f35aec619c4a01e17de525bf76dde8
unknown <ccursos@gmail.com>
2022-01-22 20:26:10 -0300

    sexto commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "hola buen dia";
+const nuevaString = "hola buen dia a todoss";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 123;
